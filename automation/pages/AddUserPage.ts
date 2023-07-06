ad709bea4aec5c3447efc17a75b76559a1e6a321
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-07-06 13:59:05 +0300

    Improved XPaths for elements on pages

--- automation/pages/AddUserPage.ts
+++ automation/pages/AddUserPage.ts
@@ -4,11 +4,9 @@ import { Locator } from "playwright-core";
 export default class AddUserPage extends BasePage {
     public userNameInput: Locator = this.page.locator("//input[@id='inputUserName']");
 
-    public yearInputValidationMessage: Locator = this.page.locator(
-        "//span[@class='text-danger field-validation-error']"
-    );
+    public yearInputValidationMessage: Locator = this.page.locator("//span[@data-id='inputError-YearOfBirth']");
 
-    public cancelButton: Locator = this.page.locator("//a[@class='btn btn-secondary']");
+    public cancelButton: Locator = this.page.locator("//a[contains(@class, 'btn btn-secondary')]");
 
-    public createButton: Locator = this.page.locator("//button[@class='btn btn-primary']");
+    public createButton: Locator = this.page.locator("//button[contains(@class, 'btn btn-primary')]");
 }
--- automation/pages/HomePage.ts
+++ automation/pages/HomePage.ts
@@ -2,7 +2,7 @@ import { Locator } from "playwright-core";
 import BasePage from "../base/page/BasePage";
 
 export default class HomePage extends BasePage {
-    public logo: Locator = this.page.locator("//a[@class='navbar-brand']");
+    public logo: Locator = this.page.locator("//a[contains(@class, 'navbar-brand')]");
 
-    public addUserButton: Locator = this.page.locator("//a[text()='Add User']");
+    public addUserButton: Locator = this.page.locator("//a[contains(., 'Add User')]");
 }

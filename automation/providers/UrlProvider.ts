46447a27730fa64d09daa47e58fcc8cf0b1be0e9
DarynaLichman <daryna.lichman@tech-stack.io>
2025-06-27 14:36:40 +0300

    Remove Client

--- automation/providers/UrlProvider.ts
+++ automation/providers/UrlProvider.ts
@@ -1,30 +1,12 @@
-import { ClientsEnum } from "../base/client/ClientsEnum";
 import appsetting from "../../appsetting.json";
 import UrlPath from "./UrlPath";
 
 export default class UrlProvider {
     public static homePageUrl(): string {
-        return appsetting.MainPageUrl;
+        return appsetting.MAIN_PAGE_URL;
     }
 
     public static urlBuilder(urlPath: UrlPath): string {
         return `${this.homePageUrl()}${urlPath}`;
-    }
-
-    public static clientUrl(clientName: ClientsEnum): string {
-        switch (clientName) {
-            case ClientsEnum.Client_1: {
-                return appsetting.Client_1_Url;
-            }
-            case ClientsEnum.Client_2: {
-                return appsetting.Client_1_Url;
-            }
-            case ClientsEnum.Client_3: {
-                return appsetting.Client_1_Url;
-            }
-            default: {
-                throw Error(`Unable to generate client URL for '${clientName}' brand`);
-            }
-        }
     }
 }
